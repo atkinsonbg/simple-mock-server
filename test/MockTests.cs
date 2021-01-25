0da297e1ec18d4d7542ef214251a194e17cb5376
Brandon Atkinson <tehremo@gmail.com>
2021-01-25 06:21:12 -0500

    Adding more tests

---
+++ test/MockTests.cs
@@ -0,0 +1,64 @@
+using System;
+using Xunit;
+using api;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Text.Json;
+
+namespace test
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IMocks, Mocks>();
+        }
+    }
+    
+    public class MockTests
+    {
+        private IMocks _mocks;
+
+        public MockTests(IMocks mocks)
+        {
+            _mocks = mocks;
+        }
+
+        [Fact]
+        public void TestBuildCollection()
+        {
+            var c = _mocks.GetCollection();
+            Assert.True(c.Count > 0);
+        }
+
+        [Fact]
+        public void TestMocksAreMounted()
+        {
+            var c = _mocks.GetCollection();
+            var m = c.FirstOrDefault();
+            Assert.True(m.RootElement.GetProperty("url").ToString() == "/mockservertest/mock1");
+        }
+
+        [Fact]
+        public void TestMockFoundByRouteStatusCode()
+        {
+            var m = _mocks.GetResponse("/mockservertest/mock1", "POST", null, null);
+            Assert.True(m.StatusCode == 200);
+        }
+
+        [Fact]
+        public void TestMockFoundByRouteContent()
+        {
+            var m = _mocks.GetResponse("/mockservertest/mock1", "GET", null, null);
+            var json = m.Content.Replace(" ","").Replace("\n", "");
+            Assert.True(json == "{\"method\":\"GET\",\"route\":\"mock2\"}");
+        }
+
+        [Fact]
+        public void TestMockRouteNotFound()
+        {
+            var m = _mocks.GetResponse("/mockservertest/mock999999999", "GET", null, null);
+            Assert.True(m.StatusCode == 499);
+        }
+    }
+}
